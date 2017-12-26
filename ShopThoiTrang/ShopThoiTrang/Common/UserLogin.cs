a711a57581346ccf4783ae8b861d5912d049013b
tuonghuan <tuonghuan10000@gmail.com>
2017-12-26 22:13:16 +0700

    Hoan tat user login

--- ShopThoiTrang/ShopThoiTrang/Common/UserLogin.cs
+++ ShopThoiTrang/ShopThoiTrang/Common/UserLogin.cs
@@ -8,7 +8,6 @@ namespace ShopThoiTrang
     [Serializable]
     public class UserLogin
     {
-
         public long UserID { get; set; }
         public string UserName { get; set; }
         public string GroupID { set; get; }
