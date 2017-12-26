867251d405bcaee685204f5535e7e2af610efcf5
tuonghuan <tuonghuan10000@gmail.com>
2017-12-26 22:12:33 +0700

    Chinh sua user login

--- ShopThoiTrang/ShopThoiTrang/Common/UserLogin.cs
+++ ShopThoiTrang/ShopThoiTrang/Common/UserLogin.cs
@@ -8,6 +8,7 @@ namespace ShopThoiTrang
     [Serializable]
     public class UserLogin
     {
+
         public long UserID { get; set; }
         public string UserName { get; set; }
         public string GroupID { set; get; }
